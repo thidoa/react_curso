1b47ac792e63e920d208aaed97e8ec59e85bd997
thidoa <thiagovini200@gmail.com>
2024-05-20 19:22:40 -0300

    feat: lifecycle methods

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,41 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
   state = {
-      name: 'Thiago Vinícius',
-      counter: 0
+    counter: 0,
+    posts: [
+      {
+        id: '1',
+        title: 'Título 1',
+        body: 'Body 1'
+      },
+      {
+        id: '2',
+        title: 'Título 2',
+        body: 'Body 2'
+      },
+      {
+        id: '3',
+        title: 'Título 3',
+        body: 'Body 3'
+      }
+    ]
   };
+  timeoutUpdate = null
 
-  handlePClick = () => {
-    this.setState({ name: 'Pirata'} )
+  componentDidMount() {
+    this.handleTimeout()
   }
 
-  handleAClick = (event) => {
-    event.preventDefault()
-    const { counter } = this.state
-    this.setState({ counter: counter + 1 })
+  componentDidUpdate() {
+    this.handleTimeout()
   }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate)
+  }
+
+  handleTimeout = () => {
+    const { counter, posts } = this.state
+    posts[0].title = 'O título mudou'
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({posts, counter: counter + 1})
+    }, 3000)
+  } 
 
   render() {
-    const { name, counter } = this.state;
+    const { posts, counter } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>{ counter }</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{ post.title }</h1>
+            <p>{ post.body }</p>
+          </div>
+        ))}
       </div>
     );
   }
